867989167dcb9d31affbf462ade09843888eba31
HuyWork <huymywork1908@gmail.com>
2024-04-15 21:33:41 +0700

    add menu screen

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
-import sys
 import random
+import math
 from tkinter import filedialog
 from tkinter import *
-import math
+from pybutton import Button
 
 pygame.init()
 vec = pygame.math.Vector2
@@ -14,29 +14,30 @@ ACC = 0.3
 FPS = 60
 FPS_CLOCK = pygame.time.Clock()
 COUNT = 0
-flags = pygame.FULLSCREEN
-displaysurface = pygame.display.set_mode((WIDTH, HEIGHT), vsync=1)
+FONT = "resources/font.ttf"
+flags = pygame.RESIZABLE
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Game")
 
-class Ground(pygame.sprite.Sprite):
+class Tree(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        self.tree = pygame.image.load("resources/images/tree.png").convert_alpha()
+
+    def render(self, x, y):
+        displaysurface.blit(self.tree, (x, y))
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, tree):
+        super().__init__()
+        self.tree = tree
         self.grass = pygame.image.load("resources/images/grass.png").convert_alpha()
     
     def render(self):
         for x in range(int(WIDTH/self.grass.get_width())+1):
             for y in range(int(HEIGHT/self.grass.get_height())+1):
                 displaysurface.blit(self.grass, (x * 100, y * 100))
-
-class Tree(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.tree = pygame.image.load("resources/images/tree.png").convert_alpha()
-        self.x = 100
-        self.y = 100
-
-    def render(self):
-        displaysurface.blit(self.tree, (self.x, self.y))
+        self.tree.render(100, 100)
 
 class Castle(pygame.sprite.Sprite):
     def __init__(self):
@@ -48,7 +49,6 @@ class Castle(pygame.sprite.Sprite):
     def render(self):
         displaysurface.blit(self.castle, (self.x, self.y))
                 
-
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -63,9 +63,12 @@ class Player(pygame.sprite.Sprite):
         self.pos = [100, 100]
         self.keys = [False, False, False, False]
 
+    def collsion(self):
+        pass
+
     def move(self):
         self.mousepos = pygame.mouse.get_pos()
-        angle = math.atan2(self.mousepos[1]-(self.pos[1]+32),self.mousepos[0]-(self.pos[0]+26))
+        angle = math.atan2(self.mousepos[1]-(self.pos[1]+32), self.mousepos[0]-(self.pos[0]+26))
         self.rot = pygame.transform.rotate(self.character, 360-angle*57.29).convert_alpha()
         self.newpos = (self.pos[0]-self.rot.get_rect().width/2, self.pos[1]-self.rot.get_rect().height/2)
 
@@ -78,7 +81,24 @@ class Player(pygame.sprite.Sprite):
         elif self.keys[3] and self.pos[0] < WIDTH - self.character.get_width():
             self.pos[0]+=5
 
-    def update(self):
+    def attack(self):
+        for arrow in self.arrows:
+            index=0
+            velx=math.cos(arrow[0])*10
+            vely=math.sin(arrow[0])*10
+            arrow[1] += velx
+            arrow[2] += vely
+            if arrow[1] < -64 or arrow[1] > 640 or arrow[2] < -64 or arrow[2] > 480:
+                self.arrows.pop(index)
+            index+=1
+            for projectile in self.arrows:
+                arrow1 = pygame.transform.rotate(self.arrow, 360-projectile[0]*57.29)
+                displaysurface.blit(arrow1, (projectile[1], projectile[2]))
+
+    def render(self):
+        displaysurface.blit(self.rot, self.newpos)
+
+    def controller(self):
         pressed_keys = pygame.key.get_pressed()
         
         if pressed_keys[K_w]:
@@ -93,32 +113,30 @@ class Player(pygame.sprite.Sprite):
         if pressed_keys[K_d]:
             self.keys[3] = True
         else: self.keys[3] = False
-            
- 
-    def collsion(self):
-        pass
+    
+    def shoot(self): 
+        self.arrows.append([math.atan2(self.mousepos[1]-(self.newpos[1]+32), self.mousepos[0]-(self.newpos[0]+26)), 
+                                self.newpos[0]+32,self.newpos[1]+32])
 
-    def attack(self):
-        for arrow in self.arrows:
-            index=0
-            velx=math.cos(arrow[0])*10
-            vely=math.sin(arrow[0])*10
-            arrow[1]+=velx
-            arrow[2]+=vely
-            if arrow[1]<-64 or arrow[1]>640 or arrow[2]<-64 or arrow[2]>480:
-                self.arrows.pop(index)
-            index+=1
-            for projectile in self.arrows:
-                arrow1 = pygame.transform.rotate(self.arrow, 360-projectile[0]*57.29)
-                displaysurface.blit(arrow1, (projectile[1], projectile[2]))
-
-    def render(self):
-        displaysurface.blit(self.rot, self.newpos)
-
+    def update(self):
+        self.move()
+        self.attack()
+        self.render()
+        self.controller()
+    
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        self.badguy = pygame.image.load("resources/images/badguy.png")
+        self.rect = self.badguy.get_rect()
+        self.pos = vec(0,0)
+        self.vel = vec(0,0)
+    
+    def move(self):
+        pass
 
+    def render(self):
+        displaysurface.blit(self.badguy, (0, 0))
 
 class EventHandler():
     def __init__(self):
@@ -146,7 +164,6 @@ class EventHandler():
     def world1(self):
         self.root.destroy()
         pygame.time.set_timer(self.enemy_generation, 2000)
-        castle.hide = True
         self.battle = True
  
     def world2(self):
@@ -155,34 +172,85 @@ class EventHandler():
     def world3(self):
         self.battle = True
 
-ground = Ground()
-castle = Castle()
+class Screen():
+    def __init__(self, ground, castle, player, displaysurface):
+        self.menu_bgimage = pygame.image.load("resources/images/menu_bg.png")
+        self.displaysurface = displaysurface
+        self.ground = ground
+        self.castle = castle
+        self.player = player
+        
+    def main_menu(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if PLAY_BUTTON.checkForInput(MOUSE_POS):
+                        self.play()
+                    if QUIT_BUTTON.checkForInput(MOUSE_POS):
+                        pygame.quit()
+                        sys.exit()
+                elif event.type == pygame.VIDEORESIZE:
+                    
+                    WINDOW_WIDTH, WINDOW_HEIGHT = event.size
+                    self.displaysurface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 
+                                                                pygame.RESIZABLE)
+            
+            self.displaysurface.fill((255, 255, 255))
+            self.displaysurface.blit(self.menu_bgimage, (0, 0))
+            pygame.display.set_caption("Menu")
+            MOUSE_POS = pygame.mouse.get_pos()
+
+            MENU_TEXT = pygame.font.Font(FONT, 45).render("MAIN MENU", True, "#b68f40")
+            MENU_RECT = MENU_TEXT.get_rect(center=(320, 50))
+
+            PLAY_BUTTON = Button(image=pygame.image.load("resources/images/button.png"), pos=(320, 150), 
+                                text_input="PLAY", font=pygame.font.Font(FONT, 25), base_color="White", hovering_color="#3b8335")
+            QUIT_BUTTON = Button(image=pygame.image.load("resources/images/button.png"), pos=(320, 250), 
+                                text_input="QUIT", font=pygame.font.Font(FONT, 25), base_color="White", hovering_color="#3b8335")
+
+            self.displaysurface.blit(MENU_TEXT, MENU_RECT)
+
+            for button in [PLAY_BUTTON, QUIT_BUTTON]:
+                button.changeColor(MOUSE_POS)
+                button.update(self.displaysurface)
+
+            pygame.display.update()
+    
+    def play(self):
+        pygame.display.set_caption("Play")
+
+        while True:
+            displaysurface.fill((255, 255, 255))
+            
+            self.ground.render()
+            self.castle.render()
+            self.player.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.player.shoot()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        self.main_menu()
+            
+            pygame.display.update()
+            FPS_CLOCK.tick(FPS)
+
 tree = Tree()
+ground = Ground(tree)
+castle = Castle()
 player = Player()
 handler = EventHandler()
+screen = Screen(ground, castle, player, displaysurface)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                handler.stage_handler()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            player.arrows.append([math.atan2(player.mousepos[1]-(player.newpos[1]+32),player.mousepos[0]-(player.newpos[0]+26))
-                                ,player.newpos[0]+32,player.newpos[1]+32])
-    
-    displaysurface.fill((255, 255, 255))
-    # Render Functions -----
+def main():
+    screen.main_menu()
 
-    ground.render()
-    castle.render()
-
-    player.attack()
-    player.move()
-    player.render()
-    player.update()
-
-    pygame.display.update()
-    FPS_CLOCK.tick(FPS)
+if __name__ == "__main__":
+    main()
